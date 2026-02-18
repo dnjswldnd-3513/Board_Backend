9cb779f5338fdb3a84e0e45accafb398136f07c6
dnjswldnd-3513 <zheld3513@gmail.com>
2026-02-19 08:08:01 +0900

    feat: Entity 추가

--- src/main/java/com/wjw/Board/entity/Post.java
+++ src/main/java/com/wjw/Board/entity/Post.java
@@ -1,4 +1,30 @@
 package com.wjw.Board.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void pre(){
+        createdAt = LocalDateTime.now();
+    }
+
 }
